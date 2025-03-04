fe0d3c7e1c5e4a42bd3e14d6d5c6ea612157288a
Talip BÖKE <talipboke13@gmail.com>
2025-03-04 10:47:16 +0300

    Initial Commit

---
+++ SevenAppsCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCase
+//
+//  Created by Talip on 4.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
